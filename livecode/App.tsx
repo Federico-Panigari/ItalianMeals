c29330fe9b5fea0e5ad5f8d0ae8943c601614c4e
Federico Panigari <federico.panigari@edu-its.it>
2026-06-16 12:48:11 +0200

    screen uploaded

--- livecode/App.tsx
+++ livecode/App.tsx
@@ -1,25 +1,27 @@
-import { View , Text, StyleSheet} from 'react-native';
-import { StatusBar} from 'expo-status-bar';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./screens/HomeScreen";
+import { DetailsScreen } from "./screens/DetailScreen";
 
-export default function App(){
-  return(
-    <View style={styles.container} >
-      <Text>Hello</Text>
-      <Text>RN renders native views, no HTML</Text>
-      
-      <StatusBar style="auto" />
-    </View>
-  )
+const Stack = createNativeStackNavigator();
+
+const linking = {
+  prefixes: [("myapp://")],
+  config: {
+    screens: {
+      Home: "home",
+      Details: "details/:id",
+    },
+  },
+};
+
+export default function App() {
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Items" }} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  statusBar: {
-    backgroundColor: '#ea1717',
-  },
-});
